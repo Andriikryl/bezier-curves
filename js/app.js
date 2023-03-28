f2ca9785aa6ccf9d184a3739a43edee8c1424ec4
Amdrii <andreyk557@gmail.com>
2023-03-28 21:20:32 +0300

    feat[canvas-size]

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 class Animation {
   constructor() {
-    this.cnv - null;
-    this.ctx - null;
+    this.cnv = null;
+    this.ctx = null;
+    this.size = { w: 0, h: 0, cx: 0, cy: 0 };
   }
   init() {
     this.createCanvas();
@@ -9,7 +10,14 @@ class Animation {
   createCanvas() {
     this.cnv = document.createElement(`canvas`);
     this.ctx = this.cnv.getContext(`2d`);
+    this.setCanvasSize();
     document.body.appendChild(this.cnv);
+  }
+  setCanvasSize() {
+    this.size.w = this.cnv.width = window.innerWidth;
+    this.size.h = this.cnv.height = window.innerHeight;
+    this.size.cx = this.size.w / 2;
+    this.size.cy = this.size.h / 2;
   }
 }
 
