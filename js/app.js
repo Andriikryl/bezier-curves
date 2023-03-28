70a837def0eb8f8fbc8862a7ab0a6b336362a08e
Amdrii <andreyk557@gmail.com>
2023-03-28 21:28:33 +0300

    feat[draw-cru]

--- js/app.js
+++ js/app.js
@@ -12,12 +12,39 @@ class Animation {
     this.ctx = this.cnv.getContext(`2d`);
     this.setCanvasSize();
     document.body.appendChild(this.cnv);
+    window.addEventListener(`resize`, () => this.setCanvasSize());
   }
   setCanvasSize() {
     this.size.w = this.cnv.width = window.innerWidth;
     this.size.h = this.cnv.height = window.innerHeight;
     this.size.cx = this.size.w / 2;
     this.size.cy = this.size.h / 2;
+  }
+  updateCurves() {
+    let curveParam = {
+      startX: 0,
+      startY: 0,
+      controlX1: this.size.cx,
+      controlY1: 0,
+      controlX2: this.size.cx,
+      controlY2: this.size.h,
+      endX: this.size.w,
+      endY: this.size.h,
+    };
+    this.drawCurve(curveParam);
+  }
+  drawCurve({
+    startX,
+    startY,
+    controlX1,
+    controlY1,
+    controlX2,
+    controlY2,
+    endX,
+    endY,
+  }) {
+    this.ctx.strokeStyle = `white`;
+    this.ctx.beginPath();
   }
 }
 
